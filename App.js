c65c644e78f91b7214736b21d76c1037f8133116
alaminhossainemon <alaminhossainemon6@gmail.com>
2021-06-04 18:28:36 -0700

    bug fix

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import Home from './components/Home/Home';
 import { NativeRouter, Route, Link } from "react-router-native";
 import Restaurant from './components/Restaurant/Restaurant';
 
-const Stack = createStackNavigator();
+
 export default function App() {
   return (
     <NativeRouter>
