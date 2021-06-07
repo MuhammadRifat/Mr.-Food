9e83c9970d23287be25d6b40718a33b9eea06914
Rahimvi <rahimvi414@gmail.com>
2021-06-07 23:57:22 +0300

    order-delivery

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import Home from './components/Home/Home';
+import OrderDelivery from './components/OrderDelivery/OrderDelivery';
+
+// AIzaSyA5VsRYN9yR6XbIlUlnIGJZaYvF2KbGtoQ
 
 export default function App() {
   return (
     <ScrollView style={styles.container}>
       <Home></Home>
+      <OrderDelivery></OrderDelivery>
     </ScrollView>
   );
 }
